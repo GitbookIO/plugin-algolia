e024e04d2a13a58cfbacf206943dca14c074fd56
Johan Preynat <johan.preynat@gmail.com>
2016-04-08 16:57:32 +0200

    Implement node code

---
+++ index.js
@@ -0,0 +1,46 @@
+var algoliasearch = require('algoliasearch');
+
+var client = null;
+var index = null;
+
+module.exports = {
+    book: {
+        assets: './assets',
+        js: [
+            'algoliasearch.min.js', 'search-algolia.js'
+        ]
+    },
+
+    hooks: {
+        init: function() {
+            // Initialize Algolia client
+            var config = this.config.get('pluginsConfig.algolia') || this.config.get('algolia');
+            client = algoliasearch(config.applicationID, config.privateKey);
+
+            // Initialize index with book's title
+            index = client.initIndex(config.index);
+
+            // Clean index for updates
+            return index.clearIndex();
+        },
+
+        page: function(page) {
+            // Don't index when not generating website or if index has not been set
+            if (this.output.name != 'website' || !index) return page;
+
+            this.log.debug.ln('index page', page.path);
+            // Transform as text
+            text = page.content.replace(/(<([^>]+)>)/ig, '');
+
+            // Add to index
+            return index.addObject({
+                url: this.output.toURL(page.path),
+                title: page.title || '',
+                body: text
+            })
+            .then(function() {
+                return page;
+            });
+        }
+    }
+};
